52343f95b92196e451a792937ab275b8d0526497
Mushukazi kazi <167205782+Mushukazi@users.noreply.github.com>
2024-06-12 20:37:47 +0530

    Update question1.js

--- question1.js
+++ question1.js
@@ -3,5 +3,20 @@
 function compareTriplet(){
    let aliecescore = 0, bobscore = 0;
    for (let i=0; i<aliece.length; i++)
-   if{aliece[i] > bob[i]}
+   if(aliece[i] > bob[i]){
+    alicepoint++;
+    console.log( "aliecepoint" + aliecepoint);
+   }
+   else if 
+
+
+or//
+
+for (let i=1; i<=3; i++){
+     
+   for (let k=1; k<=i; k++)
+   {
+    document.write("*")
+   }
+   document.write("<br>");
 }
